8aa9b01212916732ad77cdbbd8d9f52a8497dcf9
Lanre039 <rajilat1000@gmail.com>
2020-11-26 02:50:58 +0100

    change db credentials

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -5,9 +5,15 @@
 // 2. Unique credentials on development and production servers
 // 3. Unique credentials if working with multiple developers
 
-define("DB_SERVER", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "localhost");
-define("DB_NAME", "portal");
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASS", "localhost");
+// define("DB_NAME", "portal");
+
+define("DB_SERVER", "sql10.freemysqlhosting.net");
+define("DB_USER", "sql10378579");
+define("DB_PASSWORD", "9SYqgvi2pe");
+define("DB_NAME", "sql10378579");
+
 
 ?>
